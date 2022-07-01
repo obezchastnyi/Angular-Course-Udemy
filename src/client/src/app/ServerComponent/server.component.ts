17f5c27d36f997cf98848afdfc111d2fb3cf5526
Oleksii Bezchastnyi <Oleksii_Bezchastnyi@epam.com>
2022-07-01 15:58:27 +0300

    init

---
+++ src/client/src/app/ServerComponent/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'server-component',
+  templateUrl: './server.component.html',
+  styleUrls: [],
+})
+export class ServerComponent {
+  name = 'Server component';
+}
