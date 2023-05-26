a49312a77c92a061cc4aa87a13589f3901031179
Oleksii Bezchastnyi <oleksii.bezchastnyi@oneocean.com>
2023-05-26 10:09:35 -0400

    refactoring structure

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { TestingComponent } from './testing/testing.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    TestingComponent
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/client/src/app/testing/testing.component.ts
@@ -0,0 +1,88 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'testing',
+  templateUrl: './testing.component.html',
+  styleUrls: ['./testing.component.css'],
+})
+export class TestingComponent { // implements OnInit
+
+  pushLogs: Array<Date>;
+
+  togglePassword = false;
+  password = 'tuna'
+  username = '';
+
+  onPasswordToggle(): void {
+    this.togglePassword = !this.togglePassword;
+    this.pushLogs.push(new Date());
+  }
+
+  getClass(): string {
+    return 'text-white';
+  }
+
+  resetUsername(): void {
+    this.username = '';
+  }
+
+  isEmpty(): boolean {
+    return this.username.length === 0;
+  }
+
+  constructor() {
+    this.pushLogs = new Array<Date>();
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    });
+  }
+
+  keyCode: string;  
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(e: KeyboardEvent) {
+    console.log(e)
+    this.keyCode = e.key;
+    /*if (e.key === 'F12') {
+      e.preventDefault();
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "C") {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+      return false;
+    }
+    if (e.ctrlKey && e.key == "U") {
+      return false;
+    }
+    return true;*/
+  }
+
+  /*@HostListener('document:keydown', ['$event'])
+  onKeyUp(ev:KeyboardEvent) {
+    //console.log(`The user just pressed ${ev.key}!`);
+  }*/
+
+  @HostListener('document:visibilitychange', ['$event'])
+  visibilitychange(ev:any) {
+    if (document.hidden) {
+      console.log(`LOG_1_HIDDEN`);
+    } else {
+      console.log(`LOG_2_NOT_HIDDEN`);
+    }
+  }
+
+  @HostListener('document:mouseleave', ['$event'])
+  onMouseLeave(ev:any) {
+    ev = ev ? ev : window.event;
+    var from = ev.relatedTarget || ev.toElement;
+    if (!from || from.nodeName == "HTML") {
+
+      console.log(`LOG_3_MOUSE_MOVE`);
+    }
+  }
+}
