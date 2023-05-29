4e899191c43382de6767cde704f38ca259ca1696
Oleksii Bezchastnyi <oleksii.bezchastnyi@oneocean.com>
2023-05-29 14:33:01 -0400

    finish preparing course project

--- src/client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'),
+    new Recipe('Test Recipe 2', 'This is simply a test second', 'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/3:2/w_5244,h_3496,c_limit/04102023-ratatouille-lede.jpg')
+  ];
 
   constructor() { }
 
